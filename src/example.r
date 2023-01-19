b5de986835f72a4f0dd7b99310eba83126ace3c0
Nathan Stitt <nathan@stitt.org>
2023-01-19 16:02:21 -0500

    mention login & edit url

--- src/example.r
+++ src/example.r
@@ -3,6 +3,7 @@ library(httr)
 
 var.api.key <- "B6QwwssvAzxNp6JDjH" # A test analysis that uses demographic data
 # TO TEST:
+# As kinetic-researcher-01@mailinator.com  https://staging.kinetic.openstax.org/analysis/edit/1
 # curl -X POST https://staging.kinetic.openstax.org/api/v1/researcher/responses/B6QwwssvAzxNp6JDjH/fetch
 
 api_client <- ApiClient$new(basePath = "https://staging.kinetic.openstax.org/api/v1")
